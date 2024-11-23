e06744cf40ec6a1cf9c4643a45651152531a51b4
0101Programmer <baranov-v-k@yandex.ru>
2024-11-23 20:48:35 +0300

    Add files via upload

---
+++ my_test.py
@@ -0,0 +1,21 @@
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+
+string = '123.456'
+float_integer = float(string)
+float_integer *= 10
+integer = int(float_integer)
+print(str(integer)[-1])
